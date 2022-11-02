187023b1e934e8c2aa96acac27e745638c2531d0
Jaydeep Paneliya <jspaneliya1002@gmail.com>
2022-11-02 17:49:48 +0530

    Base Page Commit

---
+++ src/main/java/org/example/BasePage.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasePage {
+    protected static WebDriver driver;
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/test/java/Drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+}
